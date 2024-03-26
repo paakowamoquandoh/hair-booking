817580dc9882d95aa3f59a074d6429d6650d0b63
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2024-03-26 16:14:33 +0000

    info sections

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = ({ title, text }) => (
+const Feature = ({ title, text, img }) => (
   <div className="gpt3__features-container__feature">
+    <div>
+      <img src={img} alt="" />
+    </div>
+    <div>
     <div className="gpt3__features-container__feature-title">
       <div />
       <h1>{title}</h1>
     </div>
     <div className="gpt3__features-container_feature-text">
       <p>{text}</p>
+    </div>
     </div>
   </div>
 );
--- src/containers/whyus/WhyUs.jsx
+++ src/containers/whyus/WhyUs.jsx
@@ -16,7 +16,7 @@ const WhyUs = () => {
       <p>-Social Media Icons-</p>
     </div>
     <div className="whyMedia-container">
-      <Feature title="Bridal services" text="Build yourself and reflect your values to attract the right responses from your audience." />
+      <Feature img={advertImg} title="Bridal services" text="Build yourself and reflect your values to attract the right responses from your audience." />
       <Feature title="Wig services" text="Providing Custom digital experiences." />
       <Feature title="General salon services" text="Optimise your online using all types of digital media not limited to websites, banners and search engine." />
     </div>
