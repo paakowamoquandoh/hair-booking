b1be42d2de1e73f18d42027233fd7ab17839e40b
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2024-03-26 11:25:29 +0000

    app update

---
+++ src/components/Swiper/Swiper.jsx
@@ -0,0 +1,118 @@
+import React, { useRef } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./swiper.css";
+
+// import required modules
+import { Autoplay, Navigation } from "swiper/modules";
+
+export default function SwiperMain() {
+  const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty("--progress", 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <SwiperSlide
+        className="swiperSlide"
+          style={{
+            width: "100%",
+            height: "100%",
+            WebkitBackgroundSize: "cover",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: `url(${require("../../media/hair/h5.jpg")})`,
+          }}
+        >
+          <h1>lorem lorem</h1><br/>
+          <p>lorem ipsum dolor calc lorem lorem lorem lorem ipsum</p>
+        </SwiperSlide>
+        <SwiperSlide
+        className="swiperSlide"
+          style={{
+            width: "100%",
+            height: "100%",
+            WebkitBackgroundSize: "cover",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: `url(${require("../../media/hair/h4.jpg")})`,
+          }}
+        >
+          <h1>lorem lorem</h1><br/>
+          <p>lorem ipsum dolor calc lorem lorem lorem lorem ipsum</p>
+        </SwiperSlide>
+        <SwiperSlide
+        className="swiperSlide"
+          style={{
+            width: "100%",
+            height: "100%",
+            WebkitBackgroundSize: "cover",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: `url(${require("../../media/hair/h6.jpg")})`,
+          }}
+        >
+          <h1>lorem lorem</h1><br/>
+          <p>lorem ipsum dolor calc lorem lorem lorem lorem ipsum</p>
+        </SwiperSlide>
+        <SwiperSlide
+        className="swiperSlide"
+          style={{
+            width: "100%",
+            height: "100%",
+            WebkitBackgroundSize: "cover",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: `url(${require("../../media/hair/h7.jpg")})`,
+          }}
+        >
+          <h1>lorem lorem</h1><br/>
+          <p>lorem ipsum dolor calc lorem lorem lorem lorem ipsum</p>
+        </SwiperSlide>
+        <SwiperSlide
+          className="swiperSlide"
+          style={{
+            width: "100%",
+            height: "100%",
+            WebkitBackgroundSize: "cover",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: `url(${require("../../media/hair/h8.jpg")})`,
+          }}
+        >
+          <h1>lorem lorem</h1><br/>
+          <p>lorem ipsum dolor calc lorem lorem lorem lorem ipsum</p>
+        </SwiperSlide>
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+    </>
+  );
+}
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
 // import ai from '../../media/hair/h14.jpg';
 import emailjs from '@emailjs/browser';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 import './header.css';
+import SwiperMain from '../../components/Swiper/Swiper';
 
 
 const Header = () => {
@@ -45,22 +40,7 @@ const Header = () => {
   </div>
 
   <div className="mediaHeader-image">
-  <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-    </Swiper>
+    <SwiperMain/>
   </div>
 </div>
   )
