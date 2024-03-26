702b75ac9436d754ff75af8e6524b33fdd19c169
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2024-03-26 18:00:35 +0000

    media icons

--- src/containers/prospects/Prospects.jsx
+++ src/containers/prospects/Prospects.jsx
@@ -10,7 +10,7 @@ const Prospects = () => {
     </div>
     <div className="gpt3__possibility-content">
       <h4>Request Early Access to Get Started</h4>
-      <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
+      <h1 className="gradient__text">Join our community of <br />  passionate stylists and embark on a journey to mastering the art of bridal beauty</h1>
       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
       <h4>Request Early Access to Get Started</h4>
     </div>
--- src/containers/whyus/WhyUs.jsx
+++ src/containers/whyus/WhyUs.jsx
@@ -5,7 +5,8 @@ import LogoFeature from '../../components/feature/LogoFeature';
 import advertImg from '../../media/hair/h16.jpg';
 import advertImg1 from '../../media/hair/h3.jpg'
 import advertImg2 from '../../media/hair/paul-siewert-dnpa8k6TGRE-unsplash.jpg'
-import advertImg3 from '../../media/hair/h15.jpg'
+import advertImg3 from '../../media/hair/h15.jpg';
+import { RiFacebookFill, RiInstagramFill, RiTwitterFill, RiWhatsappFill, RiYoutubeFill } from 'react-icons/ri';
 
 const WhyUs = () => {
   return (
@@ -16,7 +17,25 @@ const WhyUs = () => {
     </div>
     <div className="whyMedia-heading">
       <h1 className="gradient__text">Join us today and unlock your potential in the world of hairdressing!</h1>
-      <p>-Social Media Icons-</p>
+      
+      <span className='icons'>
+          <a target="_blank" rel="noopener noreferrer" href="https://web.facebook.com/ItmustbeGinger">
+            <RiFacebookFill size={30}/>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="/">
+            <RiInstagramFill size={30}/>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@WendySomuah">
+            <RiYoutubeFill size={30}/>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="/">
+            <RiWhatsappFill size={30}/>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/ginger_makeupGH">
+            <RiTwitterFill size={30}/>
+          </a>
+        </span>
+      
     </div>
     <div className="whyMedia-container">
       <Feature img={advertImg1} title="Bridal services" text="Build yourself and reflect your values to attract the right responses from your audience." />
